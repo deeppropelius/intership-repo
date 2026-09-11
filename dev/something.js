d3110be99f715a6a65ec0b9b68c13e92d3b37bd3
deeppropelius <deeppropelius@gmail.com>
2026-09-11 13:01:58 +0530

    map in branch

--- dev/something.js
+++ dev/something.js
@@ -8,3 +8,11 @@ function myfunction(color) {
 }
 console.log("Function Declared Outside");
 arry.forEach(myfunction);
+var User = [
+    { name: "Deep ", age: 21 },
+    { name: "Jeeya", age: 20 }
+];
+var user = User.map(function (name) {
+    return name.name;
+});
+console.log(`Using map \n Users ${user}`);
--- dev/something.ts
+++ dev/something.ts
@@ -8,3 +8,13 @@ function myfunction (color:string) {
 }
 console.log("Function Declared Outside");
 arry.forEach(myfunction);
+
+var User =[
+    {name : "Deep ", age: 21 },
+    {name :  "Jeeya", age :20}
+];
+var user= User.map(function(name){
+    return name.name;
+
+});
+console.log(`Using map \n Users ${user}`);
