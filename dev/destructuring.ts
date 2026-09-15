6e6539e3d05462d9d92ea592ade1db00496a61d6
deeppropelius <deepproletius@gail.com>
2026-09-15 11:52:05 +0530

    Destructuring

---
+++ dev/destructuring.ts
@@ -0,0 +1,15 @@
+const User ={
+    name : "Deep",
+    age :21,
+    intern: true,
+    location:"Surat"
+}
+
+
+
+console.log("USing destructuring...");
+function printUser({name,age , location , intern }){
+    return `User Name : ${name}\nUser age : ${age}\nUser location: ${location}\nUser InternStatus: ${intern}`
+}
+
+console.log(printUser(User));
