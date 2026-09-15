1210dba0fdc3508e10ac0741d22b04faaed88450
deeppropelius <deepproletius@gail.com>
2026-09-15 09:35:16 +0530

    default arguments

---
+++ dev/default.js
@@ -0,0 +1,6 @@
+"use strict";
+function details(name = "Deep", surname = "Rajput") {
+    console.log(`Name: ${name} \nsurname : ${surname}`);
+}
+details();
+details("Chirag", "Rajput");
---
+++ dev/default.ts
@@ -0,0 +1,5 @@
+function details(name: string= "Deep" , surname: string ="Rajput"){
+    console.log(`Name: ${name} \nsurname : ${surname}`);
+}
+details();
+details("Chirag", "Rajput");
